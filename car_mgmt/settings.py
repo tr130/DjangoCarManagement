e32301d787d2b86fbe1f9ac538f98d6c1b81a947
Henry Golding <hgolding@protonmail.com>
2021-09-13 09:38:51 +0100

    ignore src

--- car_mgmt/settings.py
+++ car_mgmt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'dev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tr130.co.uk', 'www.tr130.co.uk']
+ALLOWED_HOSTS = []
 
 
 # Application definition
